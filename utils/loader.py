fixed softmax bug in wavenet optimizer

utils/loader.py
@@ -65,7 +65,13 @@ class Loader(object):
         * bucket_id: choice of the bucket to fetch from.
         * data_nums: list of sequence IDs (possibly in np.array([int]) format).
         """
-        pass # [TODO: FIX THIS]
+        signal_batch = self.pad_and_onehot_signal(
+            [self._dataset['bucket_{}'.format(bucket_id)]['signals']['{}'.format(d)][:] for d in data_nums],
+            [self._buckets_data[bucket_id]['signal_lengths'][d] for d in data_nums])
+        sequence_batch = self.pad_sequence(
+            [self._dataset['bucket_{}'.format(bucket_id)]['reads']['{}'.format(d)][:] for d in data_nums],
+            [self._buckets_data[bucket_id]['read_lengths'][d] for d in data_nums])
+        return (signal_batch, sequence_batch)
 
 
     def fetch_one(self, bucket_id, data_num):
@@ -74,7 +80,7 @@ class Loader(object):
         """
         return (self._dataset['bucket_{}'.format(bucket_id)]['signals']['{}'.format(data_num)][:],
                 self._dataset['bucket_{}'.format(bucket_id)]['reads']['{}'.format(data_num)][:])
-        
+
     ### Static Helper Functions:
     @staticmethod
     def random_bucket(num_buckets):
@@ -85,6 +91,27 @@ class Loader(object):
     def random_sequences(num_sequences, batch_size):
         """Choose a batch at random. Randomly chooses an integer list of length `batch_size` from [0,num_sequences)."""
         return np.random.choice(num_sequences, size=batch_size, replace=False)
+
+    @staticmethod
+    def pad_and_onehot_signal(signals_batch, signal_lengths):
+        """
+        Pad and one-hot-encode a list of signals.
+        
+        Args:
+        * signals_batch: a list of ndarrays; these are the signals themselves.
+        * signal_lengths: a list of integers of same length as the list `signals_batch`; these are the lengths
+          of the signals.
+
+        Returns:
+        A batch of one-hot-encoded signals padded to the length of the maximum value.
+        """
+        pass # [TODO: figure this out]
+    
+    @staticmethod
+    def pad_sequence(sequence_batch, read_lengths):
+        """Pad a list of signals."""
+        pass # [TODO: figure this out]
+
 
     ### Helper Methods:
     def _load_metadata(self):

wavenet_overfit_test.py
@@ -4,6 +4,7 @@ Overfit the core wavenet model on a single signal tensor.
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.autograd import Variable
 
 from modules.wavenet import WaveNet
@@ -13,23 +14,24 @@ num_iterations = 100000
 # load signal from data and create dataset:
 signal = torch.load("./data/overfit/one_hot_signal.pth")
 _, dense_signal = torch.max(signal[:,:,1:], dim=1)
-source_seq = signal
-target_seq = dense_signal
+source_seq = signal[:,:,0:200]
+target_seq = dense_signal[:,0:200]
 if torch.cuda.is_available():
     source_seq = source_seq.cuda()
     target_seq = target_seq.cuda()
 
 # construct model and optimizer:
+# (set softmax=False because CE loss already does softmax for you)
 dilations = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
              1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
              1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
-wavenet = WaveNet(256, 2, [(256, 256, 2, d) for d in dilations], 256, softmax=True)
+wavenet = WaveNet(256, 2, [(256, 256, 2, d) for d in dilations], 256, softmax=False)
 if torch.cuda.is_available(): wavenet.cuda()
-learning_rate = 0.001
+learning_rate = 1.0
+rho = 0.9
 wd = 0.0001
-betas = (0.9, 0.999)
-optimizer = optim.RMSprop(wavenet.parameters(), lr=learning_rate)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5)
+optimizer = optim.Adadelta(wavenet.parameters(), lr=learning_rate, rho=rho, weight_decay=wd)
+scheduler = ReduceLROnPlateau(optimizer, patience=5)
 loss_fn = nn.CrossEntropyLoss()
 
 # run training loop and occasionally print losses:
@@ -46,11 +48,10 @@ try:
         optimizer.step()
         if (step % print_every == 0):
             scalar_loss = loss.data[0]
-            print("Step: {}".format(step))
-            print("Total loss on this iteration: {}".format(scalar_loss))
-            print("Average loss per sample on this iteration: {}".format(scalar_loss / target_seq.size(1)))
+            print("Step: {0} | Total Loss: {1} | Average Per-Sample Loss: {2}".format(
+                step, scalar_loss, scalar_loss / target_seq.size(1)))
             if best_observed_loss > scalar_loss: best_observed_loss = scalar_loss
-            scheduler.step(loss)
+            scheduler.step(scalar_loss)
 except KeyboardInterrupt:
     print("....Training interrupted.")
 finally: