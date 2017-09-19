113ffc30f359e5631427ad78e99ad6beae202eaf
Paul Tang <paulswtang@gmail.com>
2017-09-19 14:10:29 -0400

    updated tests to automatically detect available GPU

--- tests/test_block.py
+++ tests/test_block.py
@@ -71,3 +71,67 @@ for step in range(num_iterations):
     loss.backward()
     nc_optimizer.step()
 print("... Done. You should see a gradually decreasing loss. (If you don't, something went wrong.)")
+
+#___________________________________________________________________________________________________
+
+##### Run CUDA tests if available:
+if not (torch.cuda.is_available()): return
+print("GPU with CUDA support detected. Running CUDA tests...")
+
+### construct input sequence:
+in_seq = Variable(torch.randn(batch_size, in_channels, sequence_length).cuda())
+
+### construct residual block and fetch outputs:
+residual_block = ResidualBlock(in_channels, out_channels, kernel_width, dilation).cuda()
+out_seq, skip_connection = residual_block(in_seq)
+
+noncausal_block = ResidualBlock(in_channels, out_channels, kernel_width, dilation, causal=False).cuda()
+nc_out_seq, nc_skip_connection = noncausal_block(in_seq)
+
+### assertions and prints:
+print("===== Assert that causal dimensions are correct... =====")
+print(out_seq.size())
+print(skip_connection.size())
+
+assert (torch.Size([batch_size, out_channels, sequence_length]) == out_seq.size())
+assert (torch.Size([batch_size, out_channels, sequence_length]) == skip_connection.size())
+
+print("===== Assert that non-causal dimensions are correct... =====")
+print(nc_out_seq.size())
+print(nc_skip_connection.size())
+
+assert (torch.Size([batch_size, out_channels, sequence_length]) == nc_out_seq.size())
+assert (torch.Size([batch_size, out_channels, sequence_length]) == nc_skip_connection.size())
+
+# run overfitting test on constant sequence:
+print("===== Attempting to overfit on constant sequences with causal block... =====")
+source_sequence = Variable(torch.ones(batch_size, in_channels, sequence_length).mul_(2.).cuda())
+target_sequence = Variable(torch.ones(batch_size, out_channels, sequence_length).mul_(3.).cuda())
+num_iterations = 1000
+print_every = 50
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(residual_block.parameters())
+for step in range(num_iterations):
+    optimizer.zero_grad()
+    outs, skips = residual_block(source_sequence)
+    loss = loss_fn(outs+skips, target_sequence)
+    if step % print_every == 0: print("Loss @ step {0}: {1}".format(step,loss.data[0]))
+    loss.backward()
+    optimizer.step()
+print("... Done. You should see a gradually decreasing loss. (If you don't, something went wrong.)")
+
+print("===== Attempting to overfit on constant sequences with non-causal block... =====")
+source_sequence = Variable(torch.ones(batch_size, in_channels, sequence_length).mul_(2.).cuda())
+target_sequence = Variable(torch.ones(batch_size, out_channels, sequence_length).mul_(3.).cuda())
+num_iterations = 1000
+print_every = 50
+loss_fn = torch.nn.MSELoss()
+nc_optimizer = torch.optim.Adam(noncausal_block.parameters())
+for step in range(num_iterations):
+    nc_optimizer.zero_grad()
+    outs, skips = noncausal_block(source_sequence)
+    loss = loss_fn(outs+skips, target_sequence)
+    if step % print_every == 0: print("Loss @ step {0}: {1}".format(step,loss.data[0]))
+    loss.backward()
+    nc_optimizer.step()
+print("... Done. You should see a gradually decreasing loss. (If you don't, something went wrong.)")
--- tests/test_conv_ops.py
+++ tests/test_conv_ops.py
@@ -65,3 +65,61 @@ for step in range(num_iterations):
     loss.backward()
     nc_optimizer.step()
 print("... Done. You should see a gradually decreasing loss.")
+
+#___________________________________________________________________________________________________
+
+##### Run CUDA tests if available:
+if not (torch.cuda.is_available()): return
+print("GPU with CUDA support detected. Running CUDA tests...")
+
+# example input seq:
+in_seq_ = torch.randn(batch_size, in_channels, seq_length).cuda()
+in_seq = Variable(in_seq_)
+
+# run causal conv1d:
+conv = CausalConv1d(in_channels, out_channels, kernel_width, dilation=dilation_rate).cuda()
+ncconv = CausalConv1d(in_channels, out_channels, kernel_width, dilation=dilation_rate).cuda()
+out_seq = conv(in_seq)
+nc_out_seq = ncconv(in_seq)
+
+# compute expected length before the conv1d:
+padding = (kernel_width-1)*dilation_rate
+expected_length = int(compute_new_length(seq_length, padding, dilation_rate, kernel_width) - padding)
+
+# assert that output is same length:
+print(out_seq.size())
+print(torch.Size([batch_size, out_channels, seq_length]))
+print(torch.Size([batch_size, out_channels, expected_length]))
+assert (torch.Size([batch_size, out_channels, seq_length]) == out_seq.size())
+assert (torch.Size([batch_size, out_channels, expected_length]) == out_seq.size())
+
+# run overfitting test on constant sequence:
+source_sequence = Variable(torch.ones(batch_size, in_channels, seq_length).mul_(2.).cuda())
+target_sequence = Variable(torch.ones(batch_size, out_channels, seq_length).mul_(3.).cuda())
+print("===== Attempting to overfit on constant sequences with causal convolution... =====")
+num_iterations = 1000
+print_every = 50
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(conv.parameters())
+for step in range(num_iterations):
+    optimizer.zero_grad()
+    preds = conv(source_sequence)
+    loss = loss_fn(preds, target_sequence)
+    if step % print_every == 0: print("Loss @ step {0}: {1}".format(step,loss.data[0]))
+    loss.backward()
+    optimizer.step()
+print("... Done. You should see a gradually decreasing loss.")
+
+print("===== Attempting to overfit on constant sequences with non-causal convolution... =====")
+num_iterations = 1000
+print_every = 50
+loss_fn = torch.nn.MSELoss()
+nc_optimizer = torch.optim.Adam(ncconv.parameters())
+for step in range(num_iterations):
+    nc_optimizer.zero_grad()
+    preds = ncconv(source_sequence)
+    loss = loss_fn(preds, target_sequence)
+    if step % print_every == 0: print("Loss @ step {0}: {1}".format(step,loss.data[0]))
+    loss.backward()
+    nc_optimizer.step()
+print("... Done. You should see a gradually decreasing loss.")
