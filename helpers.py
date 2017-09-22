a2d83c9ad93f1eca899e3fbd302f645c13bcb36f
Paul Tang <paulswtang@gmail.com>
2017-09-22 11:43:54 -0400

    added helper function to compute receptive fields

--- helpers.py
+++ helpers.py
@@ -1,31 +1,17 @@
 """
 Helper functions for training.
 """
-#import torch
-
-
-def calculate_receptive_field(dilations, kernel_width):
-    """
-    Takes a list of dilations and a kernel width (the uniform kernel size of each layer) and computes
-    the size of the receptive field, i.e. the number of timesteps in the source sequence that each
-    output timestep can observe.
-
-    Can be used for both causal and standard conv1ds (the equation is the same).
-    """
-    # base case:
-    if len(dilations) == 1:
-        d = dilations[0]
-        k = kernel_width
-        return (k * (k-1) * (d-1))
-    # recursion:
-    else:
-        calculate_receptive_field(dilations[1:], kernel_width)
-        # [TODO: figure out recursive relationship here]
-
-
 def compute_receptive_field(dilation_depth, nb_stacks):
     """
     This function is modified/borrowed from:
     https://github.com/basveeling/wavenet/blob/master/wavenet.py#L280
+
+    We implicitly assume a doubling of dilations at each step, i.e. each "stack"
+    is of the form `dilations := [1, 2, 4, ..., ]`.
+    
+    Args:
+    * dilation_depth: this is a python integer representing `len(dilations)-1` with
+    `dilations` as defined above.
+    * nb_stacks: the number of times the `dilations` block is repeated.
     """
     return nb_stacks * (2 ** dilation_depth * 2) - (nb_stacks - 1)
