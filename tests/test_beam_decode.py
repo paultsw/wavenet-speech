73dabc5749418dfa293290b900db0cf6974d12c9
Paul Tang <paulswtang@gmail.com>
2017-09-25 10:05:21 -0400

    added beam test decoded outputs to ipython notebook

--- tests/test_beam_decode.py
+++ tests/test_beam_decode.py
@@ -7,27 +7,28 @@ from modules.beam import Beam
 ### beam search settings
 beam_width = 5
 batch_size = 8
-target_dict = { '-': 0, 'a': 1, 'g': 2, 'c': 3, 't': 4, '<pad>': 5, '<s>': 6, '</s>': 7 }
+#target_dict = { '-': 0, 'a': 1, 'g': 2, 'c': 3, 't': 4, '<pad>': 5, '<s>': 6, '</s>': 7 }
+target_dict = { '<pad>': 0, 'a': 1, 'g': 2, 'c': 3, 't': 4, '<s>': 5, '</s>': 6 }
 
 ### construct a Beam object for each sequence in the batch:
 beams = [Beam(beam_width, target_dict, cuda=False) for _ in range(batch_size)]
 
 ### generate some example logits:
 sequence_length = 44
-ndim = 8 # { - A G C T <PAD> <S> </S> }
-#logits = torch.nn.functional.normalize(torch.randn(sequence_length, batch_size, ndim).abs_(), p=1, dim=2)
-logits = torch.zeros(sequence_length,batch_size,ndim)
-for k in range(sequence_length): logits[k,:,2] = 1.
+ndim = 7 #8 = { - A G C T <PAD> <S> </S> }
+logits = torch.nn.functional.normalize(torch.randn(sequence_length, batch_size, ndim).abs_(), p=1, dim=2)
+logits[:,:,5] = 0. # <BOS> == 0 for all timesteps
+logits[:,:,6] = 0. # <EOS> == 0 for all timesteps
 
 ### add <START> and <STOP> tags to the logits:
-start_vec = torch.Tensor([0,0,0,0,0,0,1,0]).view(1,1,ndim).expand(1, batch_size, ndim)
-stop_vec = torch.Tensor([0,0,0,0,0,0,0,1]).view(1,1,ndim).expand(1, batch_size, ndim)
+start_vec = torch.Tensor([0,0,0,0,0,1,0]).view(1,1,ndim).expand(1, batch_size, ndim)
+stop_vec = torch.Tensor([0,0,0,0,0,0,1]).view(1,1,ndim).expand(1, batch_size, ndim)
 logits = torch.cat([start_vec, logits, stop_vec], dim=0)
 sequence_length += 2
 
 ### loop through each timestep and update the respective beams:
 for k in range(sequence_length):
-    label_lkhd = logits[k].view(batch_size, ndim).transpose(0,1).contiguous()
+    label_lkhd = logits[k].view(batch_size, ndim)
     label_lkhd = label_lkhd.unsqueeze(1).expand(batch_size,beam_width,ndim)
     # update each beam:
     for b in range(batch_size):
